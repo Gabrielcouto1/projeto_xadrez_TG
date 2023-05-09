f657222e5c8f590263a633007f2a0c0c2a7ae604
Gabriel Couto de Freitas <gabriel.freitas.pb@compasso.com.br>
2023-05-09 16:10:30 -0300

    test: testes com o pacote de xadrez

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 const express   = require('express');
 const router    = express.Router();
+//const Chessboard= require('@chrisoakman/chessboardjs');
 
 router.get('/', (req,res)=>{
     res.render('initial-page');
@@ -7,6 +8,8 @@ router.get('/', (req,res)=>{
 
 router.get('/pagina-1', (req,res)=>{
     res.render('pagina-1');
+    //var board = Chessboard('myBoard', 'start');
+
 });
 
 router.get('/pagina-2', (req,res)=>{
